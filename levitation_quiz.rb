36c6ffb875287de85f62a57b97d4ce50fa4e3d96
jonhunter13 <jonhunter13@gmail.com>
2015-07-05 14:54:57 -0400

    levitation quiz

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -3,12 +3,10 @@ def levitation_quiz
 	#your code here
   loop do 
     puts "What is the spell that enacts levitation?"
-    answer = gets.chomp
+    answer = STDIN.gets.chomp
     break if answer == "Wingardium Leviosa"
   end
 
   puts "You passed the quiz!"
 end
 
-
-levitation_quiz
